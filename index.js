b91c0c9fa98870880bcab2b341fb2dfe5ed2da37
trakbit <harsh.vardhan.611@gmail.com>
2018-05-05 14:13:36 +0100

    beer schema created

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const router = express.Router();
+const mongoose = require('mongoose')
+const mongoDB = 'mongodb://127.0.0.1/crm';
+
+mongoose.connect(mongoDB);
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Deal Tracker Application' });
+});
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use('/api', router)
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
---
+++ models/deal.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const DealSchema = new Schema({
+    id: Number,
+    name: String,
+    amount: Number,
+    stage: Number
+})
+
+module.exports = mongoose.model({'Deal', DealSchema})

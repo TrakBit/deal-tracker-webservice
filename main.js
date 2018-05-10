9031c8af85a828e4c1024125a570d6eb086f3e7c
trakbit <harsh.vardhan.611@gmail.com>
2018-05-10 13:43:51 +0100

    enable CORS

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ router.get('/', (req, res) => {
     res.json({message: 'Deal Tracker Application'});
 });
 
+app.all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 

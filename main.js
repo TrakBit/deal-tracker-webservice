aa561ea717dfa691f4b8502eb52eddd14004e9f4
trakbit <harsh.vardhan.611@gmail.com>
2018-05-16 16:02:49 +0100

    re-enable cors

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ mongoose.connect(mongoDB, (err) => {
     }
 });
 
+app.all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+    } else {
+        next();
+    }
+});
+
 router.get('/', (req, res) => {
     res.json({message: 'Deal Tracker Application'});
 });
